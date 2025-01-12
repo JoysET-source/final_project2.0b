0bd81999f833614d7eecfdd9b275623eb1a295e2
Eugenio <joys7919@gmail.com>
2025-01-12 16:46:31 +0100

    under work stuff

--- src/routes.py
+++ src/routes.py
@@ -1,6 +1,5 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
-from typing import List
 
 from src.database import get_db
 from src.schemas import RicettaCreate, Ricetta as RicetteSchema
